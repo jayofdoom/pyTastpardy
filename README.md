218ced0bf114b42d0beeb60e6b6c8ca04fb80c54
Jay Faulkner <jay@jvf.cc>
2023-06-15 21:35:13 -0700

    Readme and mild improvements

---
+++ README.md
@@ -0,0 +1,41 @@
+pyTastpardy
+===========
+A python game bot. Mainly a personal playground for seeing how helpful AI code
+generation can be while working with libraries I'm fairly familiar with. What
+does this mean? Don't expect it to be stable/well-supported. If anyone ever
+cares to use this, open an issue or something and maybe I'll get serious :).
+
+Usage
+=====
+doesn't exist yet, but probably would look like
+
+python -m tastpardy  --config ./path/to/config.json --dbpath ./path/to/data.db
+
+Vision
+======
+Eventually I want to have a bot that does some of the following things:
+
+Commands
+--------
+* question - return a single question, and the answer shortly thereafter.
+* play - play a trivia game where your points are lost if you answer wrong
+* score [player] - send the user a notice with information about [player's]
+  score
+
+A minimal set of administrative commands will also be runnable by users with
+higher privledges. IDK what all these are yet.
+
+Chat servers supported
+----------------------
+I'm starting with IRC, because it's easy, but I'd eventually like to hook the
+commands into a discord or slack app.
+
+Question sources
+================
+Right now, I'm loading in questions from a local backup of a j-archive dump. I
+would love info on free-as-in-speech trivia question sources to use in tests
+and examples.
+
+Tests
+=====
+lol
--- tastpardy/game.py
+++ tastpardy/game.py
@@ -4,6 +4,7 @@ import uuid
 
 from sqlalchemy import func
 import irc.bot  # type: ignore
+import irc.connection  # type: ignore
 import irc.strings  # type: ignore
 
 from tastpardy.db import DBClient
@@ -24,19 +25,16 @@ class TastyBot(irc.bot.SingleServerIRCBot):
     def __init__(self, channel, nickname, server, port=6667, use_ssl=False, password=None, admins=None):
         self.password = password
         self.admins = admins
+
+        # I'll have to figure out some kind of interface to send messages to/from the game
+        # so that it'll be at least theorertically capable of running on more than just IRC
         self.game = Game()
+
         connect_params = {}
         if use_ssl:
-            connect_params["connect_factory"] = irc.connection.Factory(wrapper=ssl.wrap_socket)
+            connect_params["connect_factory"] = irc.connection.Factory(wrapper=ssl.wrap_socket)  # type: ignore
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname, None, recon=irc.bot.ExponentialBackoff(), **connect_params)
         self.channel = channel
-
-    def is_admin(self, c, e):
-        if self.admins:
-            return e.source.nick in self.admins
-        else:
-            # If no admins are specified, everyone is an admin
-            return True
 
     def on_nicknameinuse(self, c, e):
         c.nick(c.get_nickname() + "_")
@@ -76,29 +74,42 @@ class TastyBot(irc.bot.SingleServerIRCBot):
         time.sleep(30)
         c.privmsg(self.channel, "The answer was: What is {}? I hope you got it right!".format(question.answer))
 
+    def admin_command(self, nick, runner, *args):
+        # Check if nick is an admin. If we have no admins, YOLO.
+        # Warning: this means there is basically zero security if you use
+        # an IRC network with weak services.
+        if not self.admins or nick in self.admins:
+            runner(*args)
+        else:
+            raise PermissionError("You are not an admin.")
+
+    def _stats(self, c, nick):
+        for chname, chobj in self.channels.items():
+            c.notice(nick, "--- Channel statistics ---")
+            c.notice(nick, "Channel: " + chname)
+            users = sorted(chobj.users())
+            c.notice(nick, "Users: " + ", ".join(users))
+            opers = sorted(chobj.opers())
+            c.notice(nick, "Opers: " + ", ".join(opers))
+            voiced = sorted(chobj.voiced())
+            c.notice(nick, "Voiced: " + ", ".join(voiced))
+
     def do_command(self, e, cmd):
         nick = e.source.nick
         c = self.connection
 
-        if self.is_admin(c, e):
-            if cmd == "disconnect":
-                self.disconnect()
+        try:
+            if cmd == "botsnacks":
+                c.privmsg(self.channel, "It's Tasty Tasty, very very Tasty!")
+            elif cmd == "question":
+                self.single_question(c)
+            elif cmd == "disconnect":
+                self.admin_command(nick, self.disconnect)
             elif cmd == "die":
-                self.die()
+                self.admin_command(nick, self.die)
             elif cmd == "stats":
-                for chname, chobj in self.channels.items():
-                    c.notice(nick, "--- Channel statistics ---")
-                    c.notice(nick, "Channel: " + chname)
-                    users = sorted(chobj.users())
-                    c.notice(nick, "Users: " + ", ".join(users))
-                    opers = sorted(chobj.opers())
-                    c.notice(nick, "Opers: " + ", ".join(opers))
-                    voiced = sorted(chobj.voiced())
-                    c.notice(nick, "Voiced: " + ", ".join(voiced))
-        
-        if cmd == "botsnacks":
-            c.privmsg(self.channel, "It's Tasty Tasty, very very Tasty!")
-        elif cmd == "question":
-            self.single_question(c)
-        else:
-            c.notice(nick, "Not understood: " + cmd)
+                self.admin_command(nick, self._stats, c, nick)
+            else:
+                c.notice(nick, "Not tasty.")
+        except PermissionError:
+            c.notice(nick, "Not tasty.")
